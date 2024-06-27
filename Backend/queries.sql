eaf7ceb8a647949da525afdf2bc70243c8850ddd
itzpushan <pushank@iitbhilai.ac.in>
2024-06-27 11:31:41 +0530

    database upgradation

--- Backend/queries.sql
+++ Backend/queries.sql
@@ -4,3 +4,25 @@ CREATE TABLE customer(
 	first_name TEXT,
 	last_name TEXT
 );
+
+--creating food item database --
+CREATE TABLE food_item(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100),
+	price MONEY
+);
+
+--creating order database --
+CREATE TABLE orders(
+	id SERIAL PRIMARY KEY,
+	customer_id INTEGER REFERENCES customer(id),
+	is_active BOOLEAN
+);
+
+--many to many relationship with order and food_item--
+--with order_recors table --
+CREATE TABLE order_records(
+  order_id INTEGER REFERENCES orders(id),
+  food_id INTEGER REFERENCES food_item(id),
+  PRIMARY KEY (order_id, food_id)
+);
